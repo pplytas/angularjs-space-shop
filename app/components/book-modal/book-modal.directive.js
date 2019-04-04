04b464da8b1bfb19f11c52f710426f7d39c1ba3e
Panagiotis Plytas <panagiotis.plytas@gmail.com>
2019-04-04 03:33:24 +0300

    Add unit details and book functionality to bookModal

--- app/components/book-modal/book-modal.directive.js
+++ app/components/book-modal/book-modal.directive.js
@@ -9,14 +9,18 @@ angular.module('bgmarsApp')
             bookUnit: '=bookUnitFn',    // Two-way model binding
         },
         templateUrl: 'app/components/book-modal/book-modal.html',
-        controller: function($scope) {
+        controller: function($scope, $sce) {
+
+            $scope.selectedYear = null;
+            $scope.unit = {};
 
             $scope.$watch('unitId', function(unitId) {     // Trigger on selectedUnitId change
-                $scope.unit = {};
-
                 if (unitId) {
+                    $scope.unit = {};
                     $scope.getUnit(unitId).then(function(response) {
                         $scope.unit = response.data;
+
+                        $scope.unit.description = $sce.trustAsHtml($scope.unit.description);
 
                         let pictureUrl = 'http://mars.theblueground.net' + $scope.unit.pictures[0].slice(0, -9);
                         $scope.unit.picturesUrl = {
@@ -24,10 +28,30 @@ angular.module('bgmarsApp')
                             medium: pictureUrl + '-w400.jpg',
                             large: pictureUrl + '-w800.jpg'
                         }
+
+                        $scope.unit.amenitiesStr = $scope.unit.amenities.join(', ');
+
+                        let allYears = Array.from(Array(8).keys());
+                        $scope.unit.allYears = allYears.map(function(year) { return year + 2080; });
+
                         console.log($scope.unit);
                     })
                 }
             }, true);
+
+            $scope.selectYear = function(year) {
+                $scope.selectedYear = year;
+            };
+
+            $scope.getNumber = function(number) {
+                return new Array(number);
+            };
+
+            $scope.book = function(unitId, year) {
+                $scope.bookUnit(unitId, year).then(function(response) {
+                    $('#' + $scope.modalId).modal('toggle');
+                })
+            };
 
         }
     };
